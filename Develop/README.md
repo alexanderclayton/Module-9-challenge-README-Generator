32c8081bc86fa084e3fc03dd846f39e6672d353e
Alex Clayton <alexander.clayton.email@gmail.com>
2022-11-13 21:57:43 -0600

    added logic for writing file

---
+++ Develop/README.md
@@ -0,0 +1,47 @@
+# [FILENAME]
+
+## Description
+
+[DESCRIPTION]
+
+# Table of Contents
+
+* [Installation] (#installation)
+
+* [Usage] (#usage)
+
+* [License] (#license)
+
+* [Contributing] (#contributing)
+
+* [Tests] (#tests)
+
+* [Questions] (#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+[DEPENDENCIES]
+
+## Usage
+
+[USAGE]
+
+## License
+
+This project is licensed under the [LICENSE] License.
+
+## Contributing
+
+[CONTRIBUTING]
+
+## Tests
+
+To run tests, run the following command:
+
+[TESTS]
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at [EMAIL].  You can find more of my work at [[GITHUB]] (https://github.com/[GITHUB]/).
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,128 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username? ",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address? ",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your project name? ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project: ",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have? ",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSO 3", "None"],
+        default: "MIT",
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies? ",
+        default: "nmp i",
+        name: "dependencies",
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests? ",
+        default: "npm test",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo? ",
+        name: "ntk",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo? ",
+        name: "contribution",
+    },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data)  --Not sure what to do with fileName
+function writeToFile(fileName, data) {
+
+    const readMeText = `# ${data.name}
+
+## Description
+    
+${data.description}
+    
+# Table of Contents
+    
+* [Installation] (#installation)
+    
+* [Usage] (#usage)
+    
+* [License] (#license)
+    
+* [Contributing] (#contributing)
+    
+* [Tests] (#tests)
+    
+* [Questions] (#questions)
+    
+## Installation
+    
+To install necessary dependencies, run the following command:
+    
+${data.dependencies}
+    
+## Usage
+    
+${data.ntk}
+    
+## License
+    
+This project is licensed under the ${data.license} License.
+    
+## Contributing
+    
+${data.contribution}
+    
+## Tests
+    
+To run tests, run the following command:
+    
+${data.test}
+    
+## Questions
+    
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}.  You can find more of my work at [${data.github}] (https://github.com/${data.github}/).`
+
+    fs.writeFile(fileName, readMeText, (err) =>
+        err ? console.error(err) : console.log("Success!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) =>
+        //writes a filename based on the user input after the "node index.js" command
+        writeToFile(process.argv[2], data)
+        )
+
+}
 
 // Function call to initialize app
 init();
